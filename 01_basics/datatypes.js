71c9a543ea1d1e5d2907be9fd6f1624d41766c0c
Mohit Khatri <122153514+mohitkhatri13@users.noreply.github.com>
2024-09-13 13:10:52 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,23 @@
+"use strict";  // this mean our complete js code as newer version. because some code have already written in older version
+// alert(3+3)  we r using node js not browser 
+// this can be run on browser directly using console window because we already discussed that js engine is avaiable on browser
+
+
+// number - range ( 2 , to power 53)
+// bigint - for storingnarge numbers
+// string = ""
+// boolean
+// null =>standalone value   it is representation of none value eg. temperature 
+// undefined  
+// symbol - used to identify uniqueness of code
+
+
+// object
+
+console.log(typeof undefined);
+console.log(typeof null);  // null is the object 
+
+
+
+
+
---
+++ 01_basics/var.js
@@ -0,0 +1,25 @@
+const accountId =12425
+let accountEmail = "mohit@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"  // this is bad practice 
+let hello
+
+// accountId = 32352  not allowed
+
+// prefer not to use var for declaring variables
+// because it has isse regarding block scope and functional scope
+
+// console.log(accountId);
+// console.table([accountId , accountEmail , accountPassword , accountCity , hello]);
+
+{
+     let accountEmail = "dsgas";
+     var accountPassword=45465;
+   console.log(accountEmail)
+}
+console.log(accountPassword)
+
+// you can see that the variable declare with var has global scope if it is declared outside the blocks and its value changes when same name var assigned with new value but it is not true for let 
+
+
+
