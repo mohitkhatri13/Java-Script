e5345bf6837eaae15668a8bb728683ca1c026537
Mohit Khatri <122153514+mohitkhatri13@users.noreply.github.com>
2024-10-08 20:06:19 +0000

    iife

---
+++ functions/iife.js
@@ -0,0 +1,14 @@
+//immediately invoked function expression
+// aisa function jo jaise likha turank  execute ho gaya
+// EventTarget. - to connect the database and not want to pollute the local  variables from global scope variables 
+
+
+// snytax - names iife
+(function chai(){
+    console.log("db connected");
+})();
+
+// arrow fxn syntax - unnamed ife
+((name)=>{
+    console.log(`db connected2 ${name}` );
+})("mohit");
