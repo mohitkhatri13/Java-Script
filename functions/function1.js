2baa3141035f5363c7864dee1c15c435c8af04bf
Mohit Khatri <122153514+mohitkhatri13@users.noreply.github.com>
2024-10-08 14:11:51 +0000

    function1

---
+++ functions/function1.js
@@ -0,0 +1,24 @@
+function saymyname(){
+    console.log("mohit");
+}
+
+saymyname();
+
+// function add(num1 , num2){
+//     console.log( num1+ num2);
+// }
+
+function add(num1 , num2){
+    return (num1+ num2);
+}
+const result =add(1 , 3)
+console.log(`Result: ${result}`);
+
+function loginuser(username){
+    if(username ===undefined){ // (!username)
+        console.log("please enter the username")
+        return
+    }
+     return `${username} just logged in `
+}
+console.log(loginuser("mohit")) 
