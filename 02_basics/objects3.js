7da893fd4dd1a55a569977ae81c0d1c16dcdbc3a
Mohit Khatri <122153514+mohitkhatri13@users.noreply.github.com>
2024-10-08 13:33:16 +0000

    objects3

---
+++ 02_basics/objects3.js
@@ -0,0 +1,19 @@
+// destructuring of objects
+
+const course = {
+    name:"jsinhindi",
+    price:"999",
+    instructor:"hiteshsir"
+}
+
+const {instructor:ins} = course
+console.log(ins);
+
+//json data
+//  {
+//     "name":"mohit",
+//     "course":"js",
+//     "price":"free"
+// }
+
+
